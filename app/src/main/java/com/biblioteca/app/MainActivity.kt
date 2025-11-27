a51d29f3a98fe625452ae5df0789396d5e777260
Tomás Maino <mainotomasmaino@gmail.com>
2025-11-27 12:15:17 -0300

    configuracion main activitiy

---
+++ app/src/main/java/com/biblioteca/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.biblioteca.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.biblioteca.app.ui.navigation.AppNavigation
+import com.biblioteca.app.ui.theme.BibliotecaTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BibliotecaTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
+                }
+            }
+        }
+    }
+}
