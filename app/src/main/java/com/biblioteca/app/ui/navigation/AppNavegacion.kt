57c8face3d1327a669e2cd4dde040f8571728a9e
Tomás Maino <mainotomasmaino@gmail.com>
2025-11-28 12:12:40 -0300

    navegacion entre pantallas

---
+++ app/src/main/java/com/biblioteca/app/ui/navigation/AppNavegacion.kt
@@ -0,0 +1,55 @@
+package com.biblioteca.app.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.biblioteca.app.ui.screens.LoginScreen
+import com.biblioteca.app.ui.screens.PerfilScreen
+import com.biblioteca.app.ui.screens.RegistroScreen
+
+@Composable
+fun AppNavegacion() {
+    val navController = rememberNavController()
+    
+    NavHost(
+        navController = navController,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onNavegar = { ruta ->
+                    navController.navigate(ruta) {
+                        if (ruta == "prefil") {
+                            popUpTo("login") { inclusive = true }
+                        }
+                    }
+                }
+            )
+        }
+        
+        composable("regitsro") {
+            RegistroScreen(
+                onNavegar = { ruta ->
+                    navController.navigate(ruta) {
+                        if (ruta == "prefil") {
+                            popUpTo("login") { inclusive = true }
+                        }
+                    }
+                }
+            )
+        }
+        
+        composable("prefil") {
+            PerfilScreen(
+                onNavegar = { ruta ->
+                    if (ruta == "login") {
+                        navController.navigate(ruta) {
+                            popUpTo(0) { inclusive = true }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
