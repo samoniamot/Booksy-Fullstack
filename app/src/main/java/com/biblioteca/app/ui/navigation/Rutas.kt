a6f9b2d5cfe4195b9e9d4144146575cff2406744
Tomás Maino <mainotomasmaino@gmail.com>
2025-11-28 13:55:09 -0300

    arrgelo errores en rutas

--- app/src/main/java/com/biblioteca/app/ui/navigation/Rutas.kt
+++ app/src/main/java/com/biblioteca/app/ui/navigation/Rutas.kt
@@ -2,6 +2,6 @@ package com.biblioteca.app.ui.navigation
 
 sealed class Rutas(val ruta: String) {
     object Login : Rutas("login")
-    object Registro : Rutas("registro")
-    object Perfil : Rutas("perfil")
+    object Registro : Rutas("regitsro")
+    object Perfil : Rutas("prefil")
 }
