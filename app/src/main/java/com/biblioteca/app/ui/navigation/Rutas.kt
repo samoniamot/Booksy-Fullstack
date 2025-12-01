901eee15f16b8ed8ff631ff4b6487582f5030d78
Tomás Maino <mainotomasmaino@gmail.com>
2025-12-01 08:04:44 -0300

    ruta libros

--- app/src/main/java/com/biblioteca/app/ui/navigation/Rutas.kt
+++ app/src/main/java/com/biblioteca/app/ui/navigation/Rutas.kt
@@ -3,5 +3,6 @@ package com.biblioteca.app.ui.navigation
 sealed class Rutas(val ruta: String) {
     object Login : Rutas("login")
     object Registro : Rutas("regitsro")
+    object Libros : Rutas("libros")
     object Perfil : Rutas("prefil")
 }
